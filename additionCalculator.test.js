f0700a10bc0d71714efcf796e7d887cf2ae537e9
Marion Robert <mrobert148@gmail.com>
2024-02-24 22:54:18 -0500

    initial commit + create all necessary files

---
+++ additionCalculator.test.js
@@ -0,0 +1,7 @@
+// additionCalculator.test.js
+
+const additionCalculator = require("./additionCalculator");
+
+test("addition of 4 and 6 to equal 10", () => {
+  expect(additionCalculator(4, 6)).toBe(10);
+});
